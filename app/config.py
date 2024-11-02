24879f3572e28084c056ed0ed324af875ec06129
NielsJanssen90 <r0930158@student.thomasmore.be>
2024-11-02 14:53:42 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,18 +1,6 @@
 import os
 
-# Example config for MySQL using environment variables
-#DB_USERNAME = os.getenv('DB_USERNAME')
-#DB_PASSWORD = os.getenv('DB_PASSWORD')
-#DB_HOST = os.getenv('DB_HOST')
-#DB_NAME = os.getenv('DB_NAME')
-
-#SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-#SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-#import os
-
 class Config:
-    # Example config for MySQL using environment variables
     DB_USERNAME = os.getenv('DB_USERNAME')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_HOST = os.getenv('DB_HOST')
